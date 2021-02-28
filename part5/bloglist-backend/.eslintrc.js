d6131536a1f74d77b581f34d790d456d12799a25
Anmol Sinha <anmolsinha1201@outlook.com>
2021-02-28 14:57:37 +0200

    Update .eslintrc.js

--- part5/bloglist-backend/.eslintrc.js
+++ part5/bloglist-backend/.eslintrc.js
@@ -1,29 +1,29 @@
 /* eslint-disable linebreak-style */
 module.exports = {
-  env: {
-    node: true,
-    commonjs: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {
-    semi: [
-      'error',
-      'never',
-    ],
-    'linebreak-style': [
-      'error',
-      'windows',
-    ],
-    indent: [
-      'error',
-      2,
-    ],
-  },
+	env: {
+		node: true,
+		commonjs: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: [
+		'airbnb-base',
+	],
+	parserOptions: {
+		ecmaVersion: 12,
+	},
+	rules: {
+		semi: [
+			'error',
+			'never',
+		],
+		'linebreak-style': [
+			'error',
+			'windows',
+		],
+		indent: [
+			'error',
+			2,
+		],
+	},
 }
