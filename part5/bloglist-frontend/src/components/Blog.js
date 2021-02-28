363f74a5a5a45425931dc74c4bcdf4b0603454ac
Anmol Sinha <anmolsinha1201@outlook.com>
2021-02-28 15:05:50 +0200

    Completed front end

---
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const Blog = ({ blog, onLike, username, deleteBlog }) => {
+
+	const [detailedView, setDetailedView] = useState(false)
+	if (detailedView) {
+		return(
+			<div className={`blog ${blog.title}`}>
+				{blog.title} <button onClick={() => setDetailedView(false)}>hide</button> <br/>
+				{blog.url} <br/>
+				likes {blog.likes} <button onClick={() => {onLike(blog)}}>like</button><br/>
+				{blog.author}
+				{blog.user.username === username && <button onClick={() => {deleteBlog(blog)}}>delete</button>}
+			</div>
+		)
+	} else {
+		return(
+			<div className={`blog ${blog.title}`}>
+				{blog.title} {blog.author} <button onClick={() => setDetailedView(true)}>show</button> <br/>
+			</div>)
+	}
+}
+
+export default Blog
---
+++ part5/bloglist-frontend/src/components/Login.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Login = ({ handleLogin, setUsername, username, setPassword, password }) => {
+	return(
+		<form onSubmit={handleLogin}>
+			<div>
+				username
+				<input
+					id="inputUsername"
+					type="text"
+					value={username}
+					name="Username"
+					onChange={({ target }) => setUsername(target.value)}
+				/>
+			</div>
+			<div>
+				password
+				<input
+					id="inputPassword"
+					type="password"
+					value={password}
+					name="Password"
+					onChange={({ target }) => setPassword(target.value)}
+				/>
+			</div>
+			<button id="loginSubmit" type="submit">login</button>
+		</form>
+	)
+}
+
+export default Login
